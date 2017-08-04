2817f3c84343ed25eb7e817496bba06f56c8ff23
Orjiewuru Kingdom <kingdom.orjiewuru@andela.com>
2017-08-04 18:53:49 +0100

    [feature] create donation banner component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { NavigationBar } from "./NavigationBar/NavigationBar.jsx";
+import Footer from "./common/Footer.jsx";
+import DonationBanner from "./common/DonationBanner.jsx";
 
 const App = () => {
     return(
       <div className="container">
         <NavigationBar />
         <h1> Hello World </h1>
+        <DonationBanner />
+        <Footer />
       </div>
     );
 };
